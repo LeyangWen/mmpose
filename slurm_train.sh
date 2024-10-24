c24ce5b436378755337e438d98cabfa9525cf5f3
leyangwen <wenleyan@umich.edu>
2024-10-24 13:38:55 -0400

    Slurm debug

--- slurm_train.sh
+++ slurm_train.sh
@@ -31,5 +31,5 @@ python tools/train.py \
 configs/wholebody_2d_keypoint/rtmpose/VEHS7M/rtmw-l_8xb320-270e_VEHS7MOnly-384x288.py \
 --wandb_name 'Train-slurm-VEHS7MOnly' \
 --wandb_mode 'online' \
---arg_notes 'VEHS-7M only - resume'
+--arg_notes 'VEHS-7M only - resume' \
 --resume \
