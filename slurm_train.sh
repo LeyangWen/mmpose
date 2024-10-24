10d31527e10120d2284ccdad899bd0a61448af30
leyangwen <wenleyan@umich.edu>
2024-10-23 20:37:13 -0400

    Slurm debug

--- slurm_train.sh
+++ slurm_train.sh
@@ -29,7 +29,7 @@ nvidia-smi
 ## RTMPose - Train 37kpts on VEHS-7M only
 python tools/train.py \
 configs/wholebody_2d_keypoint/rtmpose/VEHS7M/rtmw-l_8xb320-270e_VEHS7MOnly-384x288.py \
---wandb_name 'Test-train-slurm' \
+--wandb_name 'Train-slurm' \
 --wandb_mode 'online' \
 --arg_notes ''
 #--resume \
