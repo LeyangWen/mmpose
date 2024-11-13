0213bb738578ba11db09d426b484bbfa31f1b007
leyangwen <wenleyan@umich.edu>
2024-11-13 10:44:00 -0500

    speed test

--- UM_dist_train.sh
+++ UM_dist_train.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=3
-#SBATCH --mem=20g
+#SBATCH --mem=60g
 #SBATCH --gres=gpu:3
 #SBATCH --time=1:00:00
 #SBATCH --account=shdpm0
